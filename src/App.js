09444ffcb1bb6b8497fa3132afbf5d078affc67b
Cesar-Morales <moraleescesar@gmail.com>
2021-01-12 18:36:29 -0300

    preparando sidebar fi

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import './App.css';
 
 function App() {
   return (
-    <div >
-      <header >
-        
-      </header>
+    <div style={position}>
+      <div style={border}>
+        <p>asdasdas</p>
+      </div>
+      <br/>
+      <div style={border}>
+        <p>asdasdas</p>
+      </div>
+      <br/>
+      <div style={border}>
+        <p>asdasdas</p>
+      </div>
     </div>
   );
+}
+const position = {
+  position: 'fixed',
+  top: '40%',
+  left: '20px',
+  border: '5px solid black'
+  
+}
+const border = {
+  border: '1px solid white'
 }
 
 export default App;
