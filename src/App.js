b1a880aed996cb1ef15e12245e02f2fb394fcdac
Cesar-Morales <moraleescesar@gmail.com>
2021-01-12 18:59:08 -0300

    esqueleto terminado

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import './App.css';
 
 function App() {
   return (
+    <div>
+    {/* navbar fixed */}   
+      <div style={card}>
+        <p>Card</p>
+      </div>
+    {/* navbar fixed */}
     <div style={position}>
       <div style={border}>
-        <p>asdasdas</p>
+        <p>home</p>
       </div>
       <br/>
       <div style={border}>
-        <p>asdasdas</p>
+        <p>page1</p>
       </div>
       <br/>
       <div style={border}>
-        <p>asdasdas</p>
+        <p>page2</p>
       </div>
+    </div>
+
+    {/* footer */}
+    <div style={footer}>
+      <div style={border}>
+      <p>Footer</p>
+      </div>
+    </div>
     </div>
   );
 }
@@ -26,6 +40,18 @@ const position = {
 }
 const border = {
   border: '1px solid white'
+}
+
+const footer = {
+  position: 'absolute',
+  bottom: '1%',
+  left: '50%'
+}
+
+const card = {
+  position: 'absolute',
+  top: '5%',
+  left: '50%'
 }
 
 export default App;
