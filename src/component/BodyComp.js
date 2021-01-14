868632456b37de00b2b569ea6e15950aa02ae998
Cesar-Morales <moraleescesar@gmail.com>
2021-01-14 16:43:16 -0300

    centrando imagen

--- src/component/BodyComp.js
+++ src/component/BodyComp.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
-import Box from '@material-ui/core/Box'
+import {Box, Grid} from '@material-ui/core'
 
 export default class BodyComp extends Component {
   render() {
     return (
       <Box  zIndex='1' position="absolute" top="28%" right="20%" left="20%" style={{background: 'white'}} >
+        <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <p style={border}></p>
+        </Grid>
+        <Grid item xs={4}>
+          <Box style={border} zIndex='3' position="absolute" left="0%" right="0%" top="-100px">
+            <img  width='200' height='200' src={process.env.PUBLIC_URL + '/images/profileImage.png'} alt="background" /> 
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <p style={border}></p>
+        </Grid>
+        </Grid>
+        <Box>TEXT</Box>
+        <Box>TEXT</Box>
+        <Box>TEXT</Box>
+        <Box>TEXT</Box>
+        <Box>TEXT</Box>
         <Box>TEXT</Box>
         <Box>TEXT</Box>
         <Box>TEXT</Box>
@@ -21,4 +39,8 @@ export default class BodyComp extends Component {
       </Box>
     )
   }
+}
+
+const border = {
+  border: '2px solid red'
 }
--- src/component/CoverPage.js
+++ src/component/CoverPage.js
@@ -5,7 +5,7 @@ import Box from '@material-ui/core/Box'
 export default class CoverPage extends Component {
     render() {
         return (
-            <Box left="0%" position="absolute"  top='0%' zIndex="0">
+            <Box left="0%" position="absolute" top='0%' zIndex="0">
                 <img width='100%' height='100%' src={process.env.PUBLIC_URL + '/images/code.jpg'} alt="background" /> 
             </Box>
         )
